f49f7f3f7039e4871f34d43a0844cc169dad3e2c
Yamil Lanz <ylanz@2050net.local>
2020-10-02 08:59:34 -0400

    Arbol de almacenes  PORFIN!!!!!

--- app/src/controllers/compras/proveedores.controller.ts
+++ app/src/controllers/compras/proveedores.controller.ts
@@ -17,112 +17,6 @@ export const insertarProveedor = async (req: Request, resp: Response) => {
     resp.status(201).json(result);
 }
 
-const buildTree = (nodo: NodoTree): NodoTree | undefined => {
-
-    if (!nodo.children) {
-        return nodo;
-    }
-    nodo.children?.push(<NodoTree>buildTree(nodo));
-
-}
-
-/* var _makeTree = function (options) {
-    var children, e, id, o, pid, temp, _i, _len, _ref;
-    id = options.idAlmacenes || "idAlmacenes";
-    pid = options.idPadre || "idPadre";
-    children = options.children || "children";
-    temp = {};
-    temp.data = {};
-    o = [];
-    _ref = options.data;
-
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-        //e.data = _ref[_i];
-        //
-        e = {};
-        e.data = _ref[_i];
-
-        //e[children] = [];
-        temp.data[e.data[id]] = e.data;
-        //console.log("identifica: ", temp);
-        console.log("temp", _ref[_i]);
-        if (temp.data[e.data[pid]] != null) {
-            e.children = [];
-            console.log("inden: ", e.data[pid]);
-            //temp.children = [];
-
-            //console.log("temp", temp);
-
-            //temp[e.data[pid]].children = [];
-            //temp.data[e.data[pid]][children].push(e);
-        } else {
-            o.push(e);
-        }
-    }
-    return o;
-};
-
-
-var _makeTreeO = function (options) {
-    var children, e, id, o, pid, temp, _i, _len, _ref;
-    id = options.idAlmacenes || "idAlmacenes";
-    pid = options.idPadre || "idPadre";
-    children = options.children || "children";
-    temp = {};
-    o = [];
-    _ref = options.data;
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-        e = _ref[_i];
-        //e[children] = [];
-        temp[e[id]] = e;
-        if (temp[e[pid]] != null) {
-            temp[e[pid]][children] = [];
-            temp[e[pid]][children].push(e);
-        } else {
-            o.push(e);
-        }
-    }
-    return o;
-}; */
-
-
-const hijosNodos = async (dato: Almacen): Promise<undefined | NodoTree[]> => {
-
-    let consulta = "SELECT * FROM almacenes WHERE idPadre = ?";
-    const hijos: Almacen[] = await db.querySelect(consulta, [dato.idAlmacenes]);
-
-    /*    let hijos: Almacen[] = [
-           { "idAlmacenes": 11, "nombre": "mtto", "descripcion": "mtto", "idPadre": 1 },
-           { "idAlmacenes": 21, "nombre": "Proyecto", "descripcion": "proyecto", "idPadre": 2 }
-       ]; */
-
-    let nodo: NodoTree = {};
-    let nodos: NodoTree[] = [];
-
-    if (hijos.length == 0) {
-        nodo.data = { ...dato };
-        return [nodo];
-    }
-
-    hijos.forEach(async (datos) => {
-        nodo.data = datos;
-        nodos.push(nodo);
-        //nodo.children?.push(<NodoTree>buildTree(nodo));
-        nodo.children?.push(<NodoTree>hijosNodos(datos));
-        nodo = {}
-    });
-
-    //awiat 
-    return nodos;
-
-};
-
-
-
-const hijoslimpios = (nodo: NodoTree) => {
-
-}
-
 const resultLimpio = (arbol: NodoTree[]) => {
     return arbol.map((dato) => {
         !dato.children && delete (dato.children);
@@ -152,13 +46,6 @@ const childNodos = (nodo: Almacen, todos: Almacen[]) => {
     return nodos;
 }
 
-/* let padresNodos: NodoTree[] = [];
-  padres.forEach((padre) => {
-      padresNodos.push({ data: padre })
-  }); */
-//(<NodoTree[]>todosBD.filter((nodo)=>nodo.idPadre == 0)).map((node)=>{data:node.data}); //
-
-
 export const todosAlmacenesArbol = async (req: Request, resp: Response) => {
 
     let consulta = "SELECT * FROM almacenes ORDER BY idPadre, idAlmacenes";
@@ -183,8 +70,5 @@ export const todosAlmacenesArbol = async (req: Request, resp: Response) => {
 
     return resp.status(201).json({ data: resultLimpio(<NodoTree[]>resultado.data) });
     //return resp.status(201).json(resultado);
-
-    //copiando
-
 
 }
