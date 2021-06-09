b424800d5c4d6ce8b8957a4fd29568353709e2e3
Yamil Lanz <ylanz@2050net.local>
2021-06-09 08:42:05 -0400

    cambios

--- app/src/interface/solpeddetalle.ts
+++ app/src/interface/solpeddetalle.ts
@@ -16,13 +16,15 @@ export default interface solpeddetalleModelo {
 	idAreaTrabajo? : number, 
 	cant_encontrada? : number, 
 	precio? : number, 
+	precio_usd?: number,
 	idProveedor? : number, 
 	notas? :string,
 	cant_recibida? : number,
 	tasa_iva?: number,
 	precio_iva?: number,
 	subtotal?: number,
-	precio_usd?: number,
-	estado?: any, //{ "proceso": 0, "aprobado": 1, "anulado": 2 }
 	precio_usd_sutotal? : number,
+	precio_neto? :number,
+	precio_neto_usd? :number,
+	estado?: any, //{ "proceso": 0, "aprobado": 1, "anulado": 2 }
 }
