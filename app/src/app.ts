f7e4e95b63ad0d22ce87f74f8d5c45800600396b
Yamil Lanz <ylanz@2050net.local>
2021-08-24 10:54:04 -0400

    10.10.0.21 whitelist

--- app/src/app.ts
+++ app/src/app.ts
@@ -3,17 +3,16 @@ import morgan from "morgan"; //trazas de consolas en las peticiones
 import cors from "cors";
 import db from "./database";
 // import { join } from "path";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 
 // dotenv.config({ path: __dirname+'/.env' });
 // dotenv.config({path: join(__dirname, '.env')});
 dotenv.config();
 
 //routes
-import comprasRoutes from "./routes/compras/compras.routes"
+import comprasRoutes from "./routes/compras/compras.routes";
 
-
-//Inicialitizations 
+//Inicialitizations
 const app = express();
 app.set("port", process.env.APP_PORT);
 // ************
@@ -25,21 +24,29 @@ app.use(express.json());
 const wl = process.env.WHITE_LIST?.split(",");
 // console.log("Whitelist: ", wl);
 
-app.use(cors({
-	origin: ["http://localhost:4200", 'http://10.10.0.16',  'http://10.10.0.7', 'http://localhost'],
-	//origin: wl,
-	credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:4200",
+      "http://10.10.0.16",
+      "http://10.10.0.7",
+      "http://localhost",
+      "http://10.10.0.21",
+    ],
+    //origin: wl,
+    credentials: true,
+  })
+);
 
-
-//rutas 
+//rutas
 app.use(comprasRoutes);
 // ************
 
 db.conectarBD();
-app.listen(app.get("port"), () => { console.log("Server express on port:", app.get("port")); });
-
+app.listen(app.get("port"), () => {
+  console.log("Server express on port:", app.get("port"));
+});
 
 app.get("/", (req, resp) => {
-	resp.send("Server http ON last!!!");
+  resp.send("Server http ON last!!!");
 });
